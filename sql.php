a10cd9df349023ccce92cb7bf43358f3ddafd001
dbursem <david@dbursem.nl>
2015-11-11 15:33:50 +0100

    Moved HTML to template files and language to language files

--- sql.php
+++ sql.php
@@ -1,10 +1,10 @@
 <?php
 class Database
 {
-    private static $dbName = '********' ;
-    private static $dbHost = '********' ;
-    private static $dbUsername = '********';
-    private static $dbUserPassword = '********';
+    private static $dbName = '******' ;
+    private static $dbHost = '******' ;
+    private static $dbUsername = '******';
+    private static $dbUserPassword = '******';
      
     private static $cont  = null;
      

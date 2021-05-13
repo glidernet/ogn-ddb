glidernet/ogn-ddb

commit 9b285c66154c708ab015c370ee5e207f315ec564

Fix SQL injection issue. Thanks NS & PC.

download/index.php
@@ -18,14 +18,14 @@ if (!empty($_GET['device_id'])) {
 }
 if (!empty($_GET['from_id'])) {
     $fromid = $_GET['from_id'];
-    $filter[] = 'dev_id >="'.$fromid.'"';
+    $filter[] = 'dev_id >= ":fromid"';
 }
 if (!empty($_GET['till_id'])) {
     $tillid = $_GET['till_id'];
     if (!empty($_GET['from_id'])) {
-      $filter[count($filter)-1] .= ' AND dev_id <="'.$tillid.'"';
+      $filter[count($filter)-1] .= ' AND dev_id <= ":tillid"';
     } else {
-      $filter[] = 'dev_id <="'.$tillid.'"';
+      $filter[] = 'dev_id <= ":tillid"';
     }
 }
 if (!empty($_GET['registration'])) {
@@ -64,6 +64,13 @@ $sql = 'SELECT
     ORDER BY dev_id ASC';
 
 $stmt = $dbh->prepare($sql);
+if (!empty($_GET['from_id'])) {
+    $stmt->bindParam('fromid', $_GET['from_id'], PDO::PARAM_STR, 12);
+}
+if (!empty($_GET['till_id'])) {
+     $stmt->bindParam('tillid', $_GET['till_id'], PDO::PARAM_STR, 12);
+}
+
 $stmt->execute($params);
 
 $output['devices'] = array();

commit 4655d860351f4a3d991f40e4d38e3cc4891b328a

Fix regression in from_id & till_id introduced in 9b285c66154c708ab015c370ee5e207f315ec564

download/index.php
@@ -17,15 +17,16 @@ if (!empty($_GET['device_id'])) {
     $params = array_merge($params, $regs);
 }
 if (!empty($_GET['from_id'])) {
-    $fromid = $_GET['from_id'];
-    $filter[] = 'dev_id >= ":fromid"';
+    $filter[] = 'dev_id >= ?';
+    array_push($params, $_GET['from_id']);
 }
 if (!empty($_GET['till_id'])) {
-    $tillid = $_GET['till_id'];
     if (!empty($_GET['from_id'])) {
-      $filter[count($filter)-1] .= ' AND dev_id <= ":tillid"';
+      $filter[count($filter)-1] .= ' AND dev_id <= ?';
+      array_push($params, $_GET['till_id']);
     } else {
-      $filter[] = 'dev_id <= ":tillid"';
+      $filter[] = 'dev_id <= ?';
+      array_push($params, $_GET['till_id']);
     }
 }
 if (!empty($_GET['registration'])) {
@@ -62,15 +63,7 @@ $sql = 'SELECT
     ON dev_actype = ac_id
     '.$filterstring.'
     ORDER BY dev_id ASC';
-
 $stmt = $dbh->prepare($sql);
-if (!empty($_GET['from_id'])) {
-    $stmt->bindParam('fromid', $_GET['from_id'], PDO::PARAM_STR, 12);
-}
-if (!empty($_GET['till_id'])) {
-     $stmt->bindParam('tillid', $_GET['till_id'], PDO::PARAM_STR, 12);
-}
-
 $stmt->execute($params);
 
 $output['devices'] = array();
7c746aea51701347aed6ef9a33d98ab27f973eef
skizzofly <alessandro.faillace@gmail.com>
2024-03-07 15:36:02 +0100

    Device expiry implementation

--- database_schema_upgrade.sql
+++ database_schema_upgrade.sql
@@ -3,6 +3,5 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-
 ALTER TABLE devices ADD COLUMN dev_updatetime bigint(20) NOT NULL;
 UPDATE devices SET dev_updatetime=UNIX_TIMESTAMP() where 1;
